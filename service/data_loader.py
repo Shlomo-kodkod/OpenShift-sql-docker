ac575126233ddbda04ae9a13ba5cc50d70812566
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-14 11:33:35 +0300

    feat: add data loader

--- service/data_loader.py
+++ service/data_loader.py
@@ -1,15 +1,34 @@
 import mysql.connector
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 
 class DataLoader:
     @staticmethod
-    def get_db():
+    def connect_to_db():
         return mysql.connector.connect(
                host="mysql",  
             user="sql-project",
             password="sqldbpass",
-            database="names"
+            database="porjdb"
         )
-            
+    
+
+    def load_data():
+        db = DataLoader.connect_to_db()
+        try:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT * FROM Names")
+                result = cursor.fetchall()
+                if not result:
+                    return None
+                logger.info("Successfully execute query")
+                return result
+        except mysql.connector.Error as e:
+            logger.info(f"Error while retrieving query: {e}")
+            return None
+        finally:
+            db.close() if db else None
     
