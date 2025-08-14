341388faf952d9cd587ca47c7e823db6d5a3d99f
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-14 11:23:00 +0300

    feat: add connect to db

--- service/data_loader.py
+++ service/data_loader.py
@@ -1,5 +1,15 @@
+import mysql.connector
 
 
 
 class DataLoader:
+    @staticmethod
+    def get_db():
+        return mysql.connector.connect(
+               host="mysql",  
+            user="sql-project",
+            password="sqldbpass",
+            database="names"
+        )
+            
     
